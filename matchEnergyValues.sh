7694334f9c47ebcb24c5e9b4029b90b4dd324aa7
judith <obermaedchen@gmail.com>
2014-05-16 14:07:16 +0200

    Add new result files and plot files

--- matchEnergyValues.sh
+++ matchEnergyValues.sh
@@ -7,25 +7,12 @@
 # HPL with 20 RPis powered -> objective = 'experiment no1'
 
 # number of experiment suites per run: 32
-# Choose min and max start time 
-# Start time of first experiment suite: 1399880531
-# Start time of last experiment suite: 1399815597
-# Start time of first experiment suite from next run: 1399881911
 
 # get experiment suite ids
-SELECT id FROM ExperimentSuite WHERE objective = 'experiment no1' AND executionStartedAt >= 1399814306 AND executionStartedAt < 1399815676;
 
-# selected ids: 71-86
-
-# first idea: get all power measurements for that time span (if any)
-
-# second idea: get only power measurements for that time span that match a hpl timestamp 
+# get only power measurements for that time span that match a hpl timestamp 
 
 
 # HPL mit 19-4 RPis powered
 # STREAM mit 20 RPis powered
 # STREAM mit 19-4 RPis powered 
-
-1399880531
-
-1399881911
