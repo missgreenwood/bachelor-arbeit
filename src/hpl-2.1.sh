10f1fed59a336c73c1e5fcd0da766b67976b2a15
judith <obermaedchen@gmail.com>
2014-05-17 23:54:06 +0200

    Update documentation chapter 3 (database configuration) and hpl execution script

--- src/hpl-2.1.sh
+++ src/hpl-2.1.sh
@@ -54,8 +54,21 @@ ENDSSH
 # log number of active RPis/powered RPis to results/hpl-2.1_`date +%y%m%d`.txt
     echo "Active RPis: $n/Powered RPis: 20" >> results/hpl-2.1_`date +%y%m%d`.txt
 
+# log experiment suite id to results/hpl-2.1_`date +%y%m%d`.txt
+echo "Experiment suite id: $myid" >> results/hpl-2.1_`date +%y%m%d`.txt
+
 # start benchmark on n RPis, log to results/hpl-2.1_`date +%y%m%d`.txt                        
     mpiexec -n $n -machinefile /srv/libraries/etc/mpich-3.0.4-shared/machinefile -wdir /srv/benchmarks/bin/hpl-2.1 /srv/benchmarks/bin/hpl-2.1/xhpl >> results/hpl-2.1_`date +%y%m%d`.txt
+
+# get end time as unix timestamp
+    endtime=`date +%s`
+    echo "End time: $endtime"
+
+# write end time of experiment suite to database                                                                                                                       
+    ssh rpi-user@careme<<ENDSSH
+mysql --user=rpi-user --password=rpiWerte rpiWerte -Bse "UPDATE ExperimentSuite SET executionEndedAt = $endtime WHERE id = $myid"                                            
+ENDSSH
+    
     echo "HPlinpack finished on $n RPis active, 20 RPis powered" 
 done 
 
@@ -109,8 +122,21 @@ ENDSSH
 # log number of active RPis/powered RPis to results/hpl-2.1_`date +%y%m%d`.txt
     echo "Active RPis: $n/Powered RPis: $m" >> results/hpl-2.1_shutdown`date +%y%m%d`.txt
 
+# log experiment suite id to results/hpl-2.1_`date +%y%m%d`.txt                                                                                                             
+    echo "Experiment suite id: $myid" >> results/hpl-2.1_shutdown`date +%y%m%d`.txt
+
 # start benchmark on n RPis, log to results/hpl-2.1_shutdown`date +%y%m%d`.txt                        
     mpiexec -n $n -machinefile /srv/libraries/etc/mpich-3.0.4-shared/machinefile -wdir /srv/benchmarks/bin/hpl-2.1 /srv/benchmarks/bin/hpl-2.1/xhpl >> results/hpl-2.1_shutdown`date +%y%m%d`.txt
+
+# get end time as unix timestamp                                                                                                                                            
+    endtime=`date +%s`
+    echo "End time: $endtime"
+
+# write end time of experiment suite to database                                                                                                                            
+    ssh rpi-user@careme<<ENDSSH
+mysql --user=rpi-user --password=rpiWerte rpiWerte -Bse "UPDATE ExperimentSuite SET executionEndedAt = $endtime WHERE id = $myid"
+ENDSSH
+
     echo "HPlinpack finished on $n RPis active, $m RPis powered"
     ssh $host 'shutdown -hP 0'
 # re-attach terminal to read new stream from STDIN
@@ -119,9 +145,58 @@ ENDSSH
     read -p "Please disconnect $host from mini usb. When finished press [Enter]"
 done 
 
-# create input file for database                                                                                                                                          
-touch results/hpl-2.1_db`date +%y%m%d`.txt
+# create database input file                                                                                                                                               touch results/hpl-2.1_db`date +%y%m%d`.txt
 
+# find all lines in first output file containing 'Experiment', 'WR' or 'end' and print to database input file                                                             
+grep -E 'Experiment|WR|end' results/hpl-2.1_`date +%y%m%d`.txt > results/hpl-2.1_db`date +%y%m%d`.txt                                                                     
+
+# find all lines in second output file containing 'Experiment', 'WR' or 'end' and print to database input file                                                             
+grep -E 'Experiment|WR|end' results/hpl-2.1_shutdown`date +%y%m%d`.txt >> results/hpl-2.1_db`date +%y%m%d`.txt                                                            
+
+# split into separate files before 'Experiment'                                                                                                                            
+awk '/Experiment/{n++}{print >"out" n ".txt" }' results/hpl-2.1_db`date +%y%m%d`.txt                                                                                      
+
+for file in out{1..32}.txt                                                                                                                                                
+do
+
+# get experiment suite id                                                                                                                                                  
+    expsuiteid=$(cat $file | cut -f 4 -d " ")
+
+# write experiment suite id to beginning of every line
+    sed -i -e "s/^/$expsuiteid /" "$file"
+
+# transform split files to lines containing space seperated values                                                                                                         
+    sed -i 's/ \{2,\}/ /g' $file
+    sed -i 'N;s/\n/ /' $file
+
+# transform split files to lines containing space seperated values                                                                                                          
+    sed -i 's/ \{2,\}/ /g' $file
+    sed -i 'N;s/\n/ /' $file
+
+    ssh rpi-user@careme<<'ENDSSH'
+    cat /srv/nfs-share/experimentsuite/$file | cut -d' ' -f1,7,8,13,14,15,16,17 | while read line
+    do
+        arr=($line)
+        time=${arr[1]}
+	echo "Time: $time"
+        gflops=${arr[2]}
+        echo "Gflops: $gflops"
+	timestamp=${arr[@]:3}
+	# echo "Timestamp: $timestamp"                                                                                                                                     
+        unixtime=$(date -d "${timestamp}" "+%s")
+        echo "Unix timestamp: $unixtime"
+        expsuiteid=${arr[0]}
+        echo "Experiment suite id: $expsuiteid"
+        mysql --user=rpi-user --password=rpiWerte rpiWerte -Bse "INSERT INTO MeasurementValue (parameter,\`value\`,measuredAt,measuredFor) VALUES ('Time',$time,$unixtime,expsuiteid)"
+        mysql --user=rpi-user --password=rpiWerte rpiWerte -Bse "INSERT INTO MeasurementValue (parameter,\`value\`,measuredAt,measuredFor) VALUES ('Gflops',$gflops,$unixtime,$expsuiteid)"
+    done
+ENDSSH
+done
+
+# remove temporary files
+rm out*
+
+# change DB input file for gnuplot
 # find all lines in first output file beginning with 'WR' (result lines) and print together with 3 following lines (timestamp lines)                                       
 grep '^WR' results/hpl-2.1_`date +%y%m%d`.txt -A 3 > results/hpl-2.1_db`date +%y%m%d`.txt
 
@@ -134,25 +209,9 @@ sed -i '/^$/d' results/hpl-2.1_db`date +%y%m%d`.txt
 # remove all lines containing "start" (only "end" needed)                                                                                                                 
 sed -i '/start/d' results/hpl-2.1_db`date +%y%m%d`.txt
 
-# remove all lines containing only '--' (result from grep -A)                                                                                                             
+# remove all lines containing only '--' (result from grep -A)                                                                                                            
 sed -i '/--/d' results/hpl-2.1_db`date +%y%m%d`.txt
 
 # transform database input file to lines containing space seperated values                                                                                                
 sed -i 's/ \{2,\}/ /g' results/hpl-2.1_db`date +%y%m%d`.txt
 sed -i 'N;s/\n/ /' results/hpl-2.1_db`date +%y%m%d`.txt
-                                                                                                                                              
-ssh rpi-user@careme<<'ENDSSH'                                                                                                                 
-cat /srv/nfs-share/experimentsuite/results/hpl-2.1_db`date +%y%m%d`.txt | cut -d' ' -f6,7,12,13,14,15 | while read line                                                   
-do                                                                                                                                                                        
-    arr=($line)                                                                                                                                                           
-    time=${arr[0]}                                                                                                                                                        
-    echo "Time: $time"                                                                                                                                                    
-    gflops=${arr[1]}                                                                                                                                                       
-    echo "Gflops: $gflops"                                                                                                                                                 
-    timestamp=${arr[@]:2}                                                                                                                                                 
-    unixtime=$(date -d "${timestamp}" "+%s")                                                                                                                              
-    echo "Timestamp: $unixtime"
-mysql --user=rpi-user --password=rpiWerte rpiWerte -Bse "INSERT INTO MeasurementValue (parameter,\`value\`,measuredAt) VALUES ('Time',$time,$unixtime)"              
-    mysql --user=rpi-user --password=rpiWerte rpiWerte -Bse "INSERT INTO MeasurementValue (parameter,\`value\`,measuredAt) VALUES ('Gflops',$gflops,$unixtime)"
-done
-ENDSSH
