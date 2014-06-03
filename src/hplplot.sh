46841d5bcd0559a80a23af5a18c89b646f74ccbb
judith <obermaedchen@gmail.com>
2014-06-03 12:14:47 +0200

    Update scripts to match energy values with benchmark timestamps

--- src/hplplot.sh
+++ src/hplplot.sh
@@ -12,8 +12,8 @@
 touch ../sources/hplplot1.txt ../sources/hplplot2.txt ../sources/tmp1.txt ../sources/tmp2.txt
 
 # set number of usable rpis
-rpis=16
-echo "Usable RPis: $rpis" 
+# rpis=16
+# echo "Usable RPis: $rpis" 
 
 linecount=$(cat ../sources/hpl-2.1_db`date +%y%m%d`.txt | wc -l)
 # linecount=$(cat ../sources/hpl-2.1_db140513.txt | wc -l)
@@ -21,8 +21,9 @@ echo "Linecount: $linecount"
 
 # split after linecount/2 lines  
 half=$((linecount / 2))
-split -l $half ../sources/hpl-2.1_db`date +%y%m%d`.txt
-# split -l $half ../sources/hpl-2.1_db140513.txt
+echo "Half of linecount: $half"
+# split -l $half ../sources/hpl-2.1_db`date +%y%m%d`.txt
+split -l 3 ../sources/hpl-2.1_db`date +%y%m%d`.txt
 
 # write to plot files sources/hplplot1.txt sources/hplplot2.txt
 cat xaa > ../sources/hplplot1.txt
@@ -32,50 +33,53 @@ cat xab > ../sources/hplplot2.txt
 cat ../sources/hplplot1.txt | cut -d' ' -f6,7 > ../sources/tmp1.txt  
 cat ../sources/hplplot2.txt | cut -d' ' -f6,7 > ../sources/tmp2.txt
 
-splitter=$((half / rpis))
+splitter=$((half / 3))
 echo "Number of lines per RPi per File: $splitter"
 
-# split tmp file 1 into rpis subfiles
-split -l $splitter ../sources/tmp1.txt plot1
+# split tmp file 1 into 3 subfiles
+split -l 1 ../sources/tmp1.txt plot1
 
-# # split tmp file 2 into rpis subfiles
-split -l $splitter ../sources/tmp2.txt plot2
+# split tmp file 2 into 3 subfiles
+split -l 1 ../sources/tmp2.txt plot2
 
 # insert number of active RPis as last column in tmp split files
 # write to plot files 
-sed -e 's/$/ 19/' plot1aa > ../sources/hplplot1.txt
-sed -e 's/$/ 18/' plot1ab >> ../sources/hplplot1.txt
-sed -e 's/$/ 17/' plot1ac >> ../sources/hplplot1.txt
-sed -e 's/$/ 16/' plot1ad >> ../sources/hplplot1.txt
-sed -e 's/$/ 15/' plot1ae >> ../sources/hplplot1.txt
-sed -e 's/$/ 14/' plot1af >> ../sources/hplplot1.txt
-sed -e 's/$/ 13/' plot1ag >> ../sources/hplplot1.txt
-sed -e 's/$/ 12/' plot1ah >> ../sources/hplplot1.txt
-sed -e 's/$/ 11/' plot1ai >> ../sources/hplplot1.txt
-sed -e 's/$/ 10/' plot1aj >> ../sources/hplplot1.txt
-sed -e 's/$/ 9/' plot1ak >> ../sources/hplplot1.txt
-sed -e 's/$/ 8/' plot1al >> ../sources/hplplot1.txt
-sed -e 's/$/ 7/' plot1am >> ../sources/hplplot1.txt
-sed -e 's/$/ 6/' plot1an >> ../sources/hplplot1.txt
-sed -e 's/$/ 5/' plot1ao >> ../sources/hplplot1.txt
-sed -e 's/$/ 4/' plot1ap >> ../sources/hplplot1.txt
 
-sed -e 's/$/ 19/' plot2aa > ../sources/hplplot2.txt
-sed -e 's/$/ 18/' plot2ab >> ../sources/hplplot2.txt
-sed -e 's/$/ 17/' plot2ac >> ../sources/hplplot2.txt
-sed -e 's/$/ 16/' plot2ad >> ../sources/hplplot2.txt
-sed -e 's/$/ 15/' plot2ae >> ../sources/hplplot2.txt
-sed -e 's/$/ 14/' plot2af >> ../sources/hplplot2.txt
-sed -e 's/$/ 13/' plot2ag >> ../sources/hplplot2.txt
-sed -e 's/$/ 12/' plot2ah >> ../sources/hplplot2.txt
-sed -e 's/$/ 11/' plot2ai >> ../sources/hplplot2.txt
-sed -e 's/$/ 10/' plot2aj >> ../sources/hplplot2.txt
-sed -e 's/$/ 9/' plot2ak >> ../sources/hplplot2.txt
-sed -e 's/$/ 8/' plot2al >> ../sources/hplplot2.txt
-sed -e 's/$/ 7/' plot2am >> ../sources/hplplot2.txt
-sed -e 's/$/ 6/' plot2an >> ../sources/hplplot2.txt
-sed -e 's/$/ 5/' plot2ao >> ../sources/hplplot2.txt
-sed -e 's/$/ 4/' plot2ap >> ../sources/hplplot2.txt
+sed -e 's/$/ 16/' plot1aa > ../sources/hplplot1.txt
+sed -e 's/$/ 8/' plot1ab >> ../sources/hplplot1.txt
+sed -e 's/$/ 4/' plot1ac >> ../sources/hplplot1.txt
+
+# sed -e 's/$/ 16/' plot1ad >> ../sources/hplplot1.txt
+# sed -e 's/$/ 15/' plot1ae >> ../sources/hplplot1.txt
+# sed -e 's/$/ 14/' plot1af >> ../sources/hplplot1.txt
+# sed -e 's/$/ 13/' plot1ag >> ../sources/hplplot1.txt
+# sed -e 's/$/ 12/' plot1ah >> ../sources/hplplot1.txt
+# sed -e 's/$/ 11/' plot1ai >> ../sources/hplplot1.txt
+# sed -e 's/$/ 10/' plot1aj >> ../sources/hplplot1.txt
+# sed -e 's/$/ 9/' plot1ak >> ../sources/hplplot1.txt
+# sed -e 's/$/ 8/' plot1al >> ../sources/hplplot1.txt
+# sed -e 's/$/ 7/' plot1am >> ../sources/hplplot1.txt
+# sed -e 's/$/ 6/' plot1an >> ../sources/hplplot1.txt
+# sed -e 's/$/ 5/' plot1ao >> ../sources/hplplot1.txt
+# sed -e 's/$/ 4/' plot1ap >> ../sources/hplplot1.txt
+
+sed -e 's/$/ 16/' plot2aa > ../sources/hplplot2.txt
+sed -e 's/$/ 8/' plot2ab >> ../sources/hplplot2.txt
+sed -e 's/$/ 4/' plot2ac >> ../sources/hplplot2.txt
+
+# sed -e 's/$/ 16/' plot2ad >> ../sources/hplplot2.txt
+# sed -e 's/$/ 15/' plot2ae >> ../sources/hplplot2.txt
+# sed -e 's/$/ 14/' plot2af >> ../sources/hplplot2.txt
+# sed -e 's/$/ 13/' plot2ag >> ../sources/hplplot2.txt
+# sed -e 's/$/ 12/' plot2ah >> ../sources/hplplot2.txt
+# sed -e 's/$/ 11/' plot2ai >> ../sources/hplplot2.txt
+# sed -e 's/$/ 10/' plot2aj >> ../sources/hplplot2.txt
+# sed -e 's/$/ 9/' plot2ak >> ../sources/hplplot2.txt
+# sed -e 's/$/ 8/' plot2al >> ../sources/hplplot2.txt
+# sed -e 's/$/ 7/' plot2am >> ../sources/hplplot2.txt
+# sed -e 's/$/ 6/' plot2an >> ../sources/hplplot2.txt
+# sed -e 's/$/ 5/' plot2ao >> ../sources/hplplot2.txt
+# sed -e 's/$/ 4/' plot2ap >> ../sources/hplplot2.txt
 
 # remove split files and tmp files 
 rm ../sources/tmp1.txt ../sources/tmp2.txt plot* xaa xab
---
+++ src/matchEnergyValuesHpl.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+
+# Match measurement values from power meter with experiment suite IDs for HPL 
+
+# number of experiment suites per run: 3
+
+# 2 experiment setups: 
+
+# HPL with 20 RPis powered 
+## objective = 'experiment no1'
+## experiment suite ids:  / (key id in table ExperimentSuite)
+
+# HPL with 20-5 RPis powered 
+## objective = 'experiment no2'
+## experiment suite ids: 344-346 (key id in table ExperimentSuite)
+
+# for each experiment setup: 
+
+## read in ids to match 
+echo -n "Please enter first experiment suite id to match and press [ENTER]: " 
+read first_expsuite
+echo -n "Please enter last experiment suite id to match and press [ENTER]: "
+read last_expsuite
+for (( c=$first_expsuite; c<=$last_expsuite; c++ ))
+do 
+# get start and end timestamps of selected experiment suites from table ExperimentSuite: keys executionStartedAt and executionEndedAt
+# get power measurement values between selected start and end timestamps from table MeasurementValue: keys measuredAt and `parameter` = 'Power'
+	mysql -u rpi-user -prpiWerte rpiWerte -Bse "SET @v1 := (SELECT executionStartedAt FROM ExperimentSuite WHERE id = $c);SET @v2 := (SELECT executionEndedAt FROM ExperimentSuite WHERE id = $c);SELECT \`value\` FROM MeasurementValue WHERE parameter = 'Power' AND measuredAt >= @v1 AND measuredAt <= @v2;" >> tmp.txt
+# get number of active RPis	 
+	mysql -u rpi-user -prpiWerte rpiWerte -Bse "SELECT \`value\` FROM ExperimentSuiteConfiguration WHERE experimentSuiteId = $c AND \`key\` = 'NumberOfActiveRPis'" >> tmp.txt
+# write delimiter to end of input 
+	echo '---' >> tmp.txt
+done
+
+# split into tmp files before delimiter
+awk '/---/{n++}{print >"out" n ".txt" }' tmp.txt
+
+# special treatment for out.txt (first split file)
+# get content of last line (number of active RPis)
+active_rpis=$(tail -1 out.txt)
+
+# remove last line from file
+sed -i '$ d' out.txt
+
+# append number of active rpis to every line in file and write to result file results/energymatch`date +%y%m%d`.txt
+cat out.txt | while read line
+do 
+	echo "$line $active_rpis" >> results/energymatch`date +%y%m%d`.txt  
+done  
+
+# same procedure for other split files 
+for i in out{1..3}.txt
+do 
+	# get content of last line (number of active RPis)
+	active_rpis=$(tail -1 $i)
+	# remove last line from file 
+	sed -i '$ d' $i
+	# remove first line from file 
+	sed -i '1d' $i 
+	# append number of active rpis to every line in file and write to result file results/energymatch`date +%y%m%d`.txt
+	cat $i | while read line 
+	do 
+		echo "$line $active_rpis" >> results/energymatch`date +%y%m%d`.txt 
+	done
+done 
+	
+# rm tmp files 
+rm out*
+rm tmp.txt
+
+# download results/energymatch`date +%y%m%d`.txt from careme 
+
+
+
+
